c220582d079135adc909bf43ebf5a1c2e39912e7
abodahmed778775-collab <abodahmed778775@gmail.com>
2026-06-26 23:54:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/api/generate', async (req, res) => {
     }
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
